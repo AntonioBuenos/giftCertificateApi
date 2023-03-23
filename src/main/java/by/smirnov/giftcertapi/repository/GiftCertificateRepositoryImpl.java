39f1f7512b5cba42e48ae1ed449159e7d2024f88
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-24 00:25:40 +0300

    Repositories accomplished

--- src/main/java/by/smirnov/giftcertapi/repository/GiftCertificateRepositoryImpl.java
+++ src/main/java/by/smirnov/giftcertapi/repository/GiftCertificateRepositoryImpl.java
@@ -2,6 +2,9 @@ package by.smirnov.giftcertapi.repository;
 
 import by.smirnov.giftcertapi.domain.GiftCertificate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,33 +13,55 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class GiftCertificateRepositoryImpl implements GiftCertificateRepository {
-    @Override
-    public GiftCertificate findById(Long id) {
-        return null;
-    }
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final GiftCertificateRowMapper giftCertificateMapper;
 
     @Override
-    public Optional<GiftCertificate> findOne(Long id) {
-        return Optional.empty();
+    public Optional<GiftCertificate> findById(Long id) {
+        return Optional.ofNullable(jdbcTemplate.queryForObject("select * from certificates.gift_certificate where id = " + id, giftCertificateMapper));
     }
 
     @Override
     public List<GiftCertificate> findAll() {
-        return null;
+        return jdbcTemplate.query("select * from certificates.gift_certificate order by id", giftCertificateMapper);
     }
 
     @Override
     public GiftCertificate create(GiftCertificate object) {
-        return null;
+        final String insertQuery =
+                "insert into certificates.gift_certificate (name, description, price, duration, create_date) " +
+                        "values (:name, :description, :price, :duration, now());";
+
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("name", object.getName());
+        mapSqlParameterSource.addValue("description", object.getDescription());
+        mapSqlParameterSource.addValue("price", object.getPrice());
+        mapSqlParameterSource.addValue("duration", object.getDuration());
+
+        namedParameterJdbcTemplate.update(insertQuery, mapSqlParameterSource);
+
+        Long lastInsertId = namedParameterJdbcTemplate.query("select currval('certificates.gift_certificate_id_seq') as last_id",
+                resultSet -> {
+                    resultSet.next();
+                    return resultSet.getLong("last_id");
+                });
+
+        return findById(lastInsertId).orElse(null);
     }
 
     @Override
     public GiftCertificate update(GiftCertificate object) {
-        return null;
+        Long id = object.getId();
+        jdbcTemplate.update(
+                "update certificates.gift_certificate " +
+                        "set name=?, description=?, price=?, duration=?, last_update_date=now() where id=?",
+                object.getName(), object.getDescription(), object.getPrice(), object.getDuration(), id);
+        return findById(id).orElse(null);
     }
 
     @Override
-    public Long delete(Long id) {
-        return null;
+    public void delete(Long id) {
+        jdbcTemplate.update("delete from certificates.gift_certificate where id=?", id);
     }
 }
--- src/main/java/by/smirnov/giftcertapi/repository/TagRepositoryImpl.java
+++ src/main/java/by/smirnov/giftcertapi/repository/TagRepositoryImpl.java
@@ -2,6 +2,9 @@ package by.smirnov.giftcertapi.repository;
 
 import by.smirnov.giftcertapi.domain.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,33 +13,50 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class TagRepositoryImpl implements TagRepository {
-    @Override
-    public Tag findById(Long id) {
-        return null;
-    }
+
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final TagRowMapper tagRowMapper;
 
     @Override
-    public Optional<Tag> findOne(Long id) {
-        return Optional.empty();
+    public Optional<Tag> findById(Long id) {
+        return Optional.ofNullable(jdbcTemplate.queryForObject("select * from certificates.tag where id = " + id, tagRowMapper));
     }
-
     @Override
     public List<Tag> findAll() {
-        return null;
+        return jdbcTemplate.query("select * from certificates.tag order by id", tagRowMapper);
     }
 
     @Override
     public Tag create(Tag object) {
-        return null;
+        final String insertQuery =
+                "insert into certificates.tag (name) " +
+                        " values (:name);";
+
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("name", object.getName());
+
+        namedParameterJdbcTemplate.update(insertQuery, mapSqlParameterSource);
+
+        Long lastInsertId = namedParameterJdbcTemplate.query("select currval('certificates.tag_id_seq') as last_id",
+                resultSet -> {
+                    resultSet.next();
+                    return resultSet.getLong("last_id");
+                });
+
+        return findById(lastInsertId).orElse(null);
     }
 
     @Override
     public Tag update(Tag object) {
-        return null;
+        Long id = object.getId();
+        jdbcTemplate.update("update certificates.tag set name=? where id=?",
+                object.getName(), id);
+        return findById(id).orElse(null);
     }
 
     @Override
-    public Long delete(Long id) {
-        return null;
+    public void delete(Long id) {
+        jdbcTemplate.update("delete from certificates.tag where id=?", id);
     }
 }
