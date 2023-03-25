fba9de79acfcc7594d7664d4ad5b2219c80f30d7
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-25 13:53:42 +0300

    GiftCertificateConverter amended

--- src/main/java/by/smirnov/giftcertapi/dto/GiftCertificateConverter.java
+++ src/main/java/by/smirnov/giftcertapi/dto/GiftCertificateConverter.java
@@ -1,7 +1,9 @@
 package by.smirnov.giftcertapi.dto;
 
 import by.smirnov.giftcertapi.domain.GiftCertificate;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface GiftCertificateConverter {
 
     GiftCertificate convert(GiftCertificateRequest request);
