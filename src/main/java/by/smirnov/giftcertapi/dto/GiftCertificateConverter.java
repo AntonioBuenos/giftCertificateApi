5f0a2e9bf8d737bc338b6297d406ae84da0aa861
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-25 14:37:44 +0300

    Duration property in GiftCertificateConverter updated

--- src/main/java/by/smirnov/giftcertapi/dto/GiftCertificateConverter.java
+++ src/main/java/by/smirnov/giftcertapi/dto/GiftCertificateConverter.java
@@ -2,10 +2,15 @@ package by.smirnov.giftcertapi.dto;
 
 import by.smirnov.giftcertapi.domain.GiftCertificate;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface GiftCertificateConverter {
 
+    @Mapping(target = "duration", expression = "java( Duration.ofDays ( request.getDuration() )")
     GiftCertificate convert(GiftCertificateRequest request);
+
+    @Mapping(target = "duration", expression = "java( request.getDuration().toDays() )")
     GiftCertificateResponse convert(GiftCertificate object);
+
 }
