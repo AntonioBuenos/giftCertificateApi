70b3ace0e8d00284f550f0a3a357e5825b9d7f1a
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-04-05 00:53:52 +0300

    TagRepositoryImpl updated

--- src/main/java/by/smirnov/giftcertapi/repository/TagRepositoryImpl.java
+++ src/main/java/by/smirnov/giftcertapi/repository/TagRepositoryImpl.java
@@ -1,11 +1,12 @@
 package by.smirnov.giftcertapi.repository;
 
+import by.smirnov.giftcertapi.domain.GiftCertificate;
 import by.smirnov.giftcertapi.domain.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,49 +15,46 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TagRepositoryImpl implements TagRepository {
 
-    private final JdbcTemplate jdbcTemplate;
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    private final TagRowMapper tagRowMapper;
+    private final SessionFactory sessionFactory;
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<Tag> findById(Long id) {
-        return Optional.ofNullable(jdbcTemplate.queryForObject("select * from certificates.tag where id = " + id, tagRowMapper));
+        Session session = sessionFactory.getCurrentSession();
+        return Optional.ofNullable(session.get(Tag.class, id));
     }
+
+    @Transactional(readOnly = true)
     @Override
     public List<Tag> findAll() {
-        return jdbcTemplate.query("select * from certificates.tag order by id", tagRowMapper);
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery(
+                "select p from Tag p order by p.id", Tag.class
+        ).getResultList();
     }
 
+    @Transactional
     @Override
     public Tag create(Tag object) {
-        final String insertQuery =
-                "insert into certificates.tag (name) " +
-                        " values (:name);";
-
-        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-        mapSqlParameterSource.addValue("name", object.getName());
-
-        namedParameterJdbcTemplate.update(insertQuery, mapSqlParameterSource);
-
-        Long lastInsertId = namedParameterJdbcTemplate.query("select currval('certificates.tag_id_seq') as last_id",
-                resultSet -> {
-                    resultSet.next();
-                    return resultSet.getLong("last_id");
-                });
-
-        return findById(lastInsertId).orElse(null);
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(object);
+        return session.get(Tag.class, object.getId());
     }
 
+    @Transactional
     @Override
     public Tag update(Tag object) {
-        Long id = object.getId();
-        jdbcTemplate.update("update certificates.tag set name=? where id=?",
-                object.getName(), id);
-        return findById(id).orElse(null);
+        Session session = sessionFactory.getCurrentSession();
+        Tag objectToBeUpdated = session.get(Tag.class, object.getId());
+        objectToBeUpdated.setName(object.getName());
+
+        return objectToBeUpdated;
     }
 
     @Override
     public void delete(Long id) {
-        jdbcTemplate.update("delete from certificates.tag where id=?", id);
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(GiftCertificate.class, id));
     }
 }
