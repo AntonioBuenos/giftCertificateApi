dda3a1ad23f6a89356e97eafb1872488ac137f1b
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-04-05 00:36:50 +0300

    Domain turned to JPA entities

--- src/main/java/by/smirnov/giftcertapi/domain/GiftCertificate.java
+++ src/main/java/by/smirnov/giftcertapi/domain/GiftCertificate.java
@@ -1,28 +1,34 @@
 package by.smirnov.giftcertapi.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.time.Duration;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "gift_certificate")
 public class GiftCertificate {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String description;
     private double price;
     private Duration duration;
     private Timestamp createDate;
+
+    @Column(name = "last_update_date")
     private Timestamp lastUpdateDate;
 }
--- src/main/java/by/smirnov/giftcertapi/domain/Tag.java
+++ src/main/java/by/smirnov/giftcertapi/domain/Tag.java
@@ -1,20 +1,23 @@
 package by.smirnov.giftcertapi.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tag")
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 }
