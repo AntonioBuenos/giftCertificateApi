bede46777138b02b20f21f682a24b85400b13bdf
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-04-09 21:20:56 +0300

    Repositories changed to Spring Data JPA

--- src/main/java/by/smirnov/giftcertapi/repository/GiftCertificateRepository.java
+++ src/main/java/by/smirnov/giftcertapi/repository/GiftCertificateRepository.java
@@ -1,6 +1,10 @@
 package by.smirnov.giftcertapi.repository;
 
 import by.smirnov.giftcertapi.domain.GiftCertificate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface GiftCertificateRepository extends CRUDRepository <Long, GiftCertificate> {
+public interface GiftCertificateRepository extends
+        CrudRepository<Long, GiftCertificate>,
+        JpaRepository <Long, GiftCertificate> {
 }
--- src/main/java/by/smirnov/giftcertapi/repository/TagRepository.java
+++ src/main/java/by/smirnov/giftcertapi/repository/TagRepository.java
@@ -1,6 +1,10 @@
 package by.smirnov.giftcertapi.repository;
 
 import by.smirnov.giftcertapi.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TagRepository extends CRUDRepository <Long, Tag> {
+public interface TagRepository extends
+        CrudRepository<Long, Tag>,
+        JpaRepository<Long, Tag> {
 }
