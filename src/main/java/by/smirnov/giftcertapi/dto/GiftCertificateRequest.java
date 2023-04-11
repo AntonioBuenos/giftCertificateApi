a0c1755b965126812e8e2da9b57fde0e88f343f5
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-04-11 23:42:13 +0300

    Validation transferred to Jakarta

--- src/main/java/by/smirnov/giftcertapi/dto/GiftCertificateRequest.java
+++ src/main/java/by/smirnov/giftcertapi/dto/GiftCertificateRequest.java
@@ -1,12 +1,13 @@
 package by.smirnov.giftcertapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
--- src/main/java/by/smirnov/giftcertapi/dto/TagRequest.java
+++ src/main/java/by/smirnov/giftcertapi/dto/TagRequest.java
@@ -1,11 +1,12 @@
 package by.smirnov.giftcertapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
